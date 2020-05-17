716db4d5e69cf83108b637cec6ee514984a85244
moses lee <moseslee90@gmail.com>
2020-05-18 00:05:58 +0800

    moved form code back to item_add

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'screens/account_add.dart';
 import 'screens/account_settings.dart';
 import 'screens/category_add.dart';
 import 'screens/item_add.dart';
-import 'screens/item_add/transaction_type_radio.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,7 +38,6 @@ class MyApp extends StatelessWidget {
           PATH_ACCOUNT_SETTINGS: (context) => AccountSettingsPage(),
           PATH_CATEGORY_ADD: (context) => AddCategoryPage(),
           PATH_ITEM_ADD: (context) => AddItemPage(),
-//          PATH_ITEM_ADD: (context) => CustomRadio(),
         },
       ),
     );
--- lib/screens/item_add.dart
+++ lib/screens/item_add.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:wallet/common/scaffold.dart';
 import 'package:wallet/models/store.dart';
 import 'package:wallet/screens/item_add/transaction_type_radio.dart';
-import './item_add/form.dart';
 import '../common/constants.dart';
 
 class AddItemPage extends StatefulWidget {
@@ -17,6 +16,7 @@ class AddItemPage extends StatefulWidget {
 }
 
 class _AddItemPageState extends State<AddItemPage> {
+  final _formKey = GlobalKey<FormState>();
   String name = '';
   int amount = 0;
   int accountId;
@@ -25,7 +25,9 @@ class _AddItemPageState extends State<AddItemPage> {
   List<RadioModel> transactionRadios;
 
   onNameChanged(value) {
-    name = value;
+    this.setState(() {
+      name = value;
+    });
   }
 
   onAmountChanged(value) {
@@ -44,24 +46,32 @@ class _AddItemPageState extends State<AddItemPage> {
     });
   }
 
+  onSubmit() {
+    print('name: $name');
+    print(
+        'amount: $amount, accountId: $accountId, categoryId: $categoryId, transactionId: $transactionType');
+  }
+
   void initState() {
     super.initState();
 
     transactionRadios = TRANSACTION_TYPE
-        .map((transInt, transStr) =>
-        MapEntry(transInt, RadioModel(
-          isSelected: false,
-          text: transStr,
-          value: transInt,
-        )))
+        .map((transInt, transStr) => MapEntry(
+            transInt,
+            RadioModel(
+              isSelected: transInt == EXPENSE_INT,
+              text: transStr,
+              value: transInt,
+            )))
         .values
         .toList();
 
     for (var i = 0; i < transactionRadios.length; i++) {
       transactionRadios[i].callback = () {
-        this.setState((){
+        this.setState(() {
           transactionRadios.forEach((element) => element.isSelected = false);
           transactionRadios[i].isSelected = true;
+          transactionType = transactionRadios[i].value;
         });
       };
     }
@@ -69,19 +79,101 @@ class _AddItemPageState extends State<AddItemPage> {
 
   @override
   Widget build(BuildContext context) {
+    StoreModel store = Provider.of<StoreModel>(context);
+
+    List<DropdownMenuItem> dropdownAccountIds = store.accounts.accounts.values
+        .map((_account) => DropdownMenuItem(
+              value: _account.id,
+              child: Text(_account.name),
+            ))
+        .toList();
+
+    List<DropdownMenuItem> dropdownCategoryIds =
+        store.categories.categories.values
+            .map((_category) => DropdownMenuItem(
+                  value: _category.id,
+                  child: Text(_category.name),
+                ))
+            .toList();
 
     // TODO: implement build
     return MyScaffold(
-      body: ExpenseForm(
-          name: name,
-          amount: amount,
-          accountId: accountId,
-          categoryId: categoryId,
-          transactionTypes: transactionRadios,
-          onNameChanged: onNameChanged,
-          onAmountChanged: onAmountChanged,
-          onAccountIdChanged: onAccountIdChanged,
-          onCategoryIdChanged: onCategoryIdChanged),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              CustomRadio(inputData: transactionRadios),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter Item Name',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                onChanged: onNameChanged,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter Item Amount',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  } else if (int.tryParse(value) == null) {
+                    return 'Please enter a number';
+                  }
+                  return null;
+                },
+                onChanged: onAmountChanged,
+              ),
+              DropdownButtonFormField(
+                value: accountId,
+                decoration: const InputDecoration(
+                  hintText: 'Select Account',
+                ),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Choose an Account';
+                  }
+                  return null;
+                },
+                items: dropdownAccountIds,
+                onChanged: onAccountIdChanged,
+              ),
+              DropdownButtonFormField(
+                value: categoryId,
+                decoration: const InputDecoration(
+                  hintText: 'Select Category',
+                ),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Choose a Category';
+                  }
+                  return null;
+                },
+                items: dropdownCategoryIds,
+                onChanged: onCategoryIdChanged,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      onSubmit();
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ])),
+      ),
     );
   }
 }
